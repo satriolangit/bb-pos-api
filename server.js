577b44309eeded702bdcee69622bad4bdd37b6fd
wahyu hari wadiantoro <wahyu.hw@gmail.com>
2022-10-07 20:10:20 +0700

    change to port 5000

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.get("/itempos", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
